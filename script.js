de05cce1c40ece0766a4f952ce03d27ffc62ecd0
Evgenii <xromenkovevgenii@mail.ru>
2019-12-20 22:57:58 +0300

    первый коммит

---
+++ script.js
@@ -0,0 +1,18 @@
+// let arr = "qwerty";
+// let rew = "asdfg";
+
+
+// console.log(arr + rew);
+// console.log(4 + "wertas");
+
+let incr = 10,
+    decr = 10;
+
+  
+    console.log(incr++);
+    console.log(decr--);
+    console.log(5%2);
+    console.log("2" === 2);
+    let isChecked = true,
+        isClose = false;
+    console.log(isChecked && isClose);
